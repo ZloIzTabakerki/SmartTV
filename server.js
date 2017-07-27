e390fb9144b8d1fde3be9c888c2896a559e3db46
Kirill <evilevilskoll@gmail.com>
2017-07-27 16:21:42 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,66 @@
+var http = require('http'),
+    fs = require('fs');
+
+httpServer = http.createServer( function (req, res) { 
+  
+  var file,
+      url = req.url,
+      method = req.method;
+  
+  //routes
+
+  if (method === 'GET' && url === '/') {
+    file = fs.readFile('./views/index.html', function (err, data) {
+      if (err) {
+        console.log('err');
+      }
+
+      res.writeHead(202, {"Content-type":"text/html"});
+      res.write(data);
+      res.end();
+      
+    });
+  } else if (method = 'GET' && url.indexOf('/assets') === 0) {
+
+    //static files server in 'assets' folder
+    
+    fileLoc = '.' + req.url;
+
+    file = fs.readFile(fileLoc, function (err, data) {
+
+      // function for getting MIMEs of static files: takes request URL, returns MIME-type
+
+      function getMIME(url) {
+        var types = {
+              jpeg: 'image/JPEG',
+              jpg: 'image/JPG',
+              css: 'text/css',
+            }, 
+            type = url.slice(url.lastIndexOf('.') + 1);
+
+        return types[type] ? types[type] : 'text/plain';
+      }
+
+      if (err) {
+        res.writeHead(404);
+        res.end('Not found');
+        console.dir(err);
+      }
+      res.writeHead(202, {"Content-type": getMIME(url)});
+      res.end(data);
+    });
+
+  } else {
+
+    //404-requests handling
+
+    res.writeHead(404);
+    res.write('Not found!');
+    res.end();
+  }
+
+});
+
+httpServer.listen(8080, function () {
+    console.log('Started on 8080!');
+});
