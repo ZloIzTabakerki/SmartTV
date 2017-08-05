afb14500ae73b23fe9f5a1e939049c4eec283ce1
evg-m <evg-m@users.noreply.github.com>
2017-08-05 17:57:52 +0300

    Added CRUD for watchlist and json file

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const express = require('express'),
       app = express(),
 
       port = process.env.PORT || 3000,
-      dbPath = __dirname + '/db/states.json';
+      statesPath = __dirname + '/db/states.json'; // < --- RENAME !
+      watchlistPath = __dirname + '/db/watchlist.json';
+
 
 //middlewares
 
@@ -14,12 +16,14 @@ app.use(bodyParser.json());
 app.use('/assets', express.static('./assets'));
 app.set('view engine', 'jade');
 
+// console.log(watchList);
+
 // function for updating DB
 
 function updateDB(req) {
   
   function writeDBData(data) {
-    fs.writeFile(dbPath, data, (err) => {
+    fs.writeFile(statesPath, data, (err) => {
       if (err) {
         console.log(err);
       }      
@@ -39,7 +43,7 @@ function updateDB(req) {
 
 function sendDBIndexData(res) {
   
-  fs.readFile(dbPath, (err, data) => {
+  fs.readFile(statesPath, (err, data) => {
 
     if (err) {
       console.log(err);
@@ -77,6 +81,150 @@ app.get('/channels', (req, res) => {
   res.json(channelsList);
 
 });
+
+// watchlist api
+
+app.get('/watchlist', (req, res) => {
+
+  fs.readFile(watchlistPath, (err, data) => {
+
+    if (err) {
+      return console.log(err);
+    }
+
+    data = JSON.parse(data.toString('utf8'));
+    if (Object.keys(req.query).length === 0){
+        res.send(data);
+    } else {
+        res.send(data[req.query.id]);
+    }
+
+  });
+
+});
+
+app.post('/watchlist/new', (req, res) => {
+
+  if (Object.keys(req.query).length === 0 || Object.keys(req.query).length > 1){
+    res.sendStatus(400);
+  } else {
+    fs.readFile(watchlistPath, (err, data) => {
+
+      if (err) {
+        return console.log(err);
+      }
+
+      data = JSON.parse(data.toString('utf8'));
+
+      if (!data[req.query.id]){
+
+      data[req.query.id] = req.body;
+      data = JSON.stringify(data);
+
+      fs.writeFile(watchlistPath, data, 'utf8', function(err) {
+      if(err) {
+        return console.log(err);
+      }
+      }); 
+
+      res.sendStatus(200);
+      } else {
+        res.sendStatus(400);
+      }
+
+    });
+
+  }
+
+});
+
+app.put('/watchlist/', (req, res) => {
+
+  if (Object.keys(req.query).length === 0 || Object.keys(req.query).length > 1){
+    res.sendStatus(400);
+  } else {
+    fs.readFile(watchlistPath, (err, data) => {
+
+      if (err) {
+        return console.log(err);
+      }
+
+      data = JSON.parse(data.toString('utf8'));
+
+      if (data[req.query.id]){
+
+      data[req.query.id] = req.body;
+      data = JSON.stringify(data);
+
+      fs.writeFile(watchlistPath, data, 'utf8', function(err) {
+      if(err) {
+        return console.log(err);
+      }
+      }); 
+
+      res.sendStatus(200);
+      
+      } else {
+
+        res.sendStatus(400);
+
+      }
+
+    });
+
+  }
+
+});
+
+app.delete('/watchlist/', (req, res) => {
+
+    if (Object.keys(req.query).length === 0 || Object.keys(req.query).length > 1){
+
+      res.sendStatus(400);
+
+    } else {
+
+      fs.readFile(watchlistPath, (err, data) => {
+
+        if (err) {
+          console.log(err);
+        }
+
+        data = JSON.parse(data.toString('utf8'));
+
+        if (data[req.query.id]){
+
+        delete data[req.query.id];
+        data = JSON.stringify(data);
+
+        fs.writeFile(watchlistPath, data, 'utf8', function(err) {
+          if(err) {
+            return console.log(err);
+          }
+        }); 
+
+        res.sendStatus(200);
+
+        } else {
+          res.sendStatus(400);
+        }
+
+      });
+
+    }
+
+});
+
+// app.get('/watchlist/new', (req, res) => {
+
+
+// });
+
+// app.get('/watchlist/edit', (req, res) => {
+
+// });
+
+//________________________________________
 
 app.listen(port, () => {
   console.log('Started on ' + port + '!');
