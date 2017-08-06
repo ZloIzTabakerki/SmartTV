e354ad478168eeadee45d31c405a9a0bbc3594b0
evg-m <evg-m@users.noreply.github.com>
2017-08-06 16:05:30 +0300

    Rewritten put and delete

--- server.js
+++ server.js
@@ -84,8 +84,6 @@ app.get('/channels', (req, res) => {
 
 app.get('/watchlist', (req, res) => {
 
-  // let watchlist = require('./db/watchlist.json');
-
   fs.readFile(watchlistPath, (err, data) => {
 
       if (err) {
@@ -94,32 +92,16 @@ app.get('/watchlist', (req, res) => {
       res.send(data.toString("UTF-8"));
   });
 
-
-  // if (Object.keys(req.query).length === 0){
-
-  //       res.send(watchlist);
-
-  // } else {
-
-  //       res.send(watchlist[req.query.id]);
-
-  // }
-
 });
 
 app.get('/watchlist/:id', function(req, res) {
     res.send(wl[req.params.id]);
-    console.log(wl[req.params.id]);
 });
 
 app.post('/watchlist/new', (req, res) => {
 
-  // let watchlist = require('./db/watchlist.json');
-
   let ndata = {};
   ndata.name = req.body.name;
-
-  // channelID !== channelId !== channel
   ndata.channelId = req.body.channelId;
   ndata.time = req.body.time;
 
@@ -131,130 +113,51 @@ app.post('/watchlist/new', (req, res) => {
 
   writeData(watchlistPath, wl);
 
-  //request accomplished conformation
   res.send();
-
-  // fs.writeFile(watchlistPath, JSON.stringify(wl), {'flags': 'a+'});
-
-  // res.sendStatus(200);
-
-  // if (Object.keys(req.query).length === 0 || Object.keys(req.query).length > 1){
-  //   res.sendStatus(400);
-  // } else {
-  //   fs.readFile(watchlistPath, (err, data) => {
-
-  //     if (err) {
-  //       return console.log(err);
-  //     }
-
-  //     data = JSON.parse(data.toString('utf8'));
-
-  //     if (!data[req.query.id]){
-
-  //     data[req.query.id] = req.body;
-  //     data = JSON.stringify(data);
-
-  //     fs.writeFile(watchlistPath, data, 'utf8', function(err) {
-  //     if(err) {
-  //       return console.log(err);
-  //     }
-  //     }); 
-
-  //     res.sendStatus(200);
-  //     } else {
-  //       res.sendStatus(400);
-  //     }
-
-  //   });
-
-  // }
 
 });
 
-// app.put('/watchlist/', (req, res) => {
+app.put('/watchlist/:id', (req, res) => {
 
-//     fs.readFile(watchlistPath, (err, data) => {
+    fs.readFile(watchlistPath, (err, data) => {
 
-//       if (err) {
-//         return console.log(err);
-//       }
+      if (err) {
+        return console.log(err);
+      }
 
-//       data = JSON.parse(data.toString('utf8'));
+      data = JSON.parse(data.toString('utf8'));
 
-//       if (data[req.body.id]){
+      let ndata = {};
+      ndata.name = req.body.name;
+      ndata.channelId = req.body.channelId;
+      ndata.time = req.body.time;
 
-//       data[req.query.id] = req.body;
-//       data = JSON.stringify(data);
+      wl[req.params.id] = ndata;
 
-//       fs.writeFile(watchlistPath, data, 'utf8', function(err) {
-//       if(err) {
-//         return console.log(err);
-//       }
-//       }); 
+        wl.sort(function(a,b){
+          return new Date(a.time) > new Date(b.time) ? 1 : -1;
+        });
 
-//       res.sendStatus(200);
+      writeData(watchlistPath, wl);
+
+      res.sendStatus(200);
       
-//       } else {
-
-//         res.sendStatus(400);
-
-//       }
-
-//     });
-
+    });
+});
   
 
-// });
+app.delete('/watchlist/:id', (req, res) => {
 
-// app.delete('/watchlist/', (req, res) => {
+      if (req.params.id > -1) {
+          wl.splice(req.params.id, 1);
+      }
 
-//     if (Object.keys(req.query).length === 0 || Object.keys(req.query).length > 1){
+      writeData(watchlistPath, wl);
 
-//       res.sendStatus(400);
+      res.sendStatus(200);
 
-//     } else {
+});
 
-//       fs.readFile(watchlistPath, (err, data) => {
-
-//         if (err) {
-//           console.log(err);
-//         }
-
-//         data = JSON.parse(data.toString('utf8'));
-
-//         if (data[req.query.id]){
-
-//         delete data[req.query.id];
-//         data = JSON.stringify(data);
-
-//         fs.writeFile(watchlistPath, data, 'utf8', function(err) {
-//           if(err) {
-//             return console.log(err);
-//           }
-//         }); 
-
-//         res.sendStatus(200);
-
-//         } else {
-//           res.sendStatus(400);
-//         }
-
-//       });
-
-//     }
-
-// });
-
-// app.get('/watchlist/new', (req, res) => {
-
-
-// });
-
-// app.get('/watchlist/edit', (req, res) => {
-
-// });
-
-//________________________________________
 
 app.listen(port, () => {
   console.log('Started on ' + port + '!');
